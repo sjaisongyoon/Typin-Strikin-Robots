07903172e71083f0890f62043d3a2b796e3d8940
Kenneth Choi <kennethichoi@gmail.com>
2019-07-14 14:58:01 -0700

    Update header component

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HeaderContainer from '../header/header_container';
 
 class MainPage extends React.Component {
 
     render() {
         return (
             <div>
+                <HeaderContainer />
                 <h1>Typin-Strikin Robots</h1>
                 <footer>
                 </footer>
