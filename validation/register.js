ddd586d45b304c212d215d4208bab2cd552691e1
kallmeehkev <kevin.la79@gmail.com>
2019-07-14 15:47:20 -0700

    removed confirm password validation

--- validation/register.js
+++ validation/register.js
@@ -6,14 +6,13 @@ module.exports = function validateRegisterInput(data) {
 
   data.username = validText(data.username) ? data.username : '';
   data.password = validText(data.password) ? data.password : '';
-  data.password2 = validText(data.password2) ? data.password2 : '';
 
   if (Validator.isEmpty(data.username)) {
     errors.username = 'Username field is required';
   }
 
-  if (!Validator.isLength(data.username, { min: 6, max: 30 })) {
-    errors.username = 'Username must be at least 6 characters';
+  if (!Validator.isLength(data.username, { min: 1, max: 30 })) {
+    errors.username = 'Username must be at least 1 character';
   }
 
   if (Validator.isEmpty(data.password)) {
@@ -22,14 +21,6 @@ module.exports = function validateRegisterInput(data) {
 
   if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = 'Password must be at least 6 characters';
-  }
-
-  if (Validator.isEmpty(data.password2)) {
-    errors.password2 = 'Confirm Password field is required';
-  }
-
-  if (!Validator.equals(data.password, data.password2)) {
-    errors.password2 = 'Passwords must match';
   }
 
   return {
