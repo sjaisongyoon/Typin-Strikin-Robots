2cc341bbb4f41fa314117ab7293a63b1f738740d
Sam Yoon <sjaisongyoon@gmail.com>
2019-07-14 16:48:13 -0700

    Add font style and users reducer

---
+++ frontend/src/reducers/users_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_USERS } from '../actions/user_actions';
+
+const usersReducer = (state={}, action) => {
+    debugger;
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_USERS:
+            return Object.assign({}, state, action.users)
+        default:
+            return state;
+    }
+};
+
+export default usersReducer;
