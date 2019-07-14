b940799a2eaba6b25125f4cec81f73e17e5409e5
Sam Yoon <sjaisongyoon@gmail.com>
2019-07-14 13:56:46 -0700

    Add session-reducer and additional API util calls

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,7 +1,25 @@
-import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+import { RECEIVE_USER_LOGOUT, 
+         RECEIVE_CURRENT_USER,
+         RECEIVE_USER_SIGN_IN } from '../actions/session_actions';
+
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+}
 
 export default function (state = initialState, action) {
     switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return {
+                ...state,
+                isAuthenticated: !!action.currentUser,
+                user: action.currentUser
+            }
+        case RECEIVE_USER_SIGN_IN:
+            return {
+                ...state,
+                isSignedIn: true
+            }
         case RECEIVE_USER_LOGOUT:
             return {
                 isAuthenticated: false,
--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -8,3 +8,10 @@ export const setAuthToken = token => {
     }
 };
 
+export const signup = (userData) => {
+    return axios.post('/api/users/register', userData);
+};
+
+export const login = (userData) => {
+    return axios.post('/api/users/login', userData);
+};
