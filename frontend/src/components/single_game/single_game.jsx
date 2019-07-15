01a3cd976cb994b7b20cdc77b43a0b763bf5f803
Kenneth Choi <kennethichoi@gmail.com>
2019-07-15 14:55:29 -0700

    Update span text rendering

--- frontend/src/components/single_game/single_game.jsx
+++ frontend/src/components/single_game/single_game.jsx
@@ -4,7 +4,6 @@ export class SingleGame extends Component {
   constructor(props) {
     super(props);
 
-
     this.state = {
       gameTime: this.props.gameTime,
       initialWords: [],
@@ -19,6 +18,7 @@ export class SingleGame extends Component {
     this.handleInput = this.handleInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.createWordsDisplay = this.createWordsDisplay.bind(this);
+    this.updateUserOutput = this.updateUserOutput.bind(this);
   }
 
   createWordsDisplay() {
@@ -40,8 +40,17 @@ export class SingleGame extends Component {
 
 
   createWordsArray() {
-    let initialWords = this.props.gamePassage.split(' ');
-    let wordCount = initialWords.length;
+    let words = this.props.gamePassage.split(' ');
+    let wordCount = words.length;
+
+    let initialWords = words.map((word, idx) => {
+      if (idx === (wordCount - 1)) {
+        return `${word}`;
+      } else {
+        return `${word} `; 
+      }
+    });
+    
     let currentWord = initialWords.shift();
 
     this.setState({
@@ -52,29 +61,49 @@ export class SingleGame extends Component {
 
   }
 
-   handleInput(e) {
-     if (e.key !== ' ') {
-      console.log(e.target.value);
-      this.setState({
-        currentInput: e.currentTarget.value
+   async handleInput(e) {
+     let key = e.key;
+     let wordSoFar = e.target.value;
+
+    //  if (key !== ' ') {
+      await this.setState({
+        currentInput: wordSoFar
       });
-    } else {
+      this.updateUserOutput();
+    // } else {
       this.handleSubmit();
-    }
+    // }
+  }
+  
+  updateUserOutput() {
+    return this.state.currentInput;
   }
 
   handleSubmit() {
     // update initialWords, correctWords, currentWord
     // clear input
     let { currentWord, currentInput} = this.state;
-    console.log('handlesubmit triggered')
+    console.log('handlesubmit triggered');
+    console.log(currentWord);
+    console.log(currentInput);
+    console.log(currentWord === currentInput);
+    
     if (currentWord === currentInput) {
+      let correctWords = [...this.state.correctWords];
+      correctWords.push(this.state.currentWord);
+      let lastCorrectIdx = [...this.state.correctWords].length;
+
+      // Update class for correct Words
+      let word = document.getElementById(`${lastCorrectIdx}`);
+      word.classList.add('word__span--correct')
+      
       this.setState({
         currentInput: '',
         initialWords: this.state.initialWords.slice(1),
-        correctWords: this.state.correctWords.push(this.state.initialWords[0]),
-        currentWord: this.state.initialWords[1]
+        correctWords: correctWords,
+        currentWord: this.state.initialWords[0]
       })
+      console.log(this.state);
     }
   }
 
@@ -82,6 +111,11 @@ export class SingleGame extends Component {
   render() {
   
     let {currentUser, gamePassage, gameTime} = this.props;
+
+    if (this.state.wordCount === this.state.correctWords.length) {
+      console.log('game over!');
+    }
+
     return (
       <div className="singlegame__container">
         <div className="singlegame__top">
@@ -107,16 +141,18 @@ export class SingleGame extends Component {
           </div>
 
           {/* TESTING */}
-          <p>{this.state.currentInput}</p>
-          {/* {this.updateUserOutput()} */}
           {/* TESTING END */}
 
           <div className="game__input-box-outer">
+            <p className="game__input-preview">
+              {this.state.currentInput === '' ? '       ' : this.state.currentInput}
+            </p>
             <input 
               type="text" 
               className="game__input-box" 
               placeholder="Type here.." 
-              onKeyDown={this.handleInput}/>
+              value={this.state.currentInput}
+              onChange={(e) => this.handleInput(e)}/>
           </div>
         </div>
       </div>
