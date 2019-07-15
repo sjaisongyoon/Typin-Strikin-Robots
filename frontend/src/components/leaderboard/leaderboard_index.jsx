a7022e79083f64821f68e1b648a17759dcfdf92b
Kenneth Choi <kennethichoi@gmail.com>
2019-07-14 21:49:29 -0700

    Update leaderboard view

--- frontend/src/components/leaderboard/leaderboard_index.jsx
+++ frontend/src/components/leaderboard/leaderboard_index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class LeaderboardIndex extends Component {
   
@@ -8,13 +9,125 @@ class LeaderboardIndex extends Component {
     if (boardType === 'single') {
       return (
         <div className="leaderboard__board-container">
-          single
+          <div className="leaderboard__table">
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">RANK</div>
+              <div className="leaderboard__table-item">WPM</div>
+              <div className="leaderboard__table-item">NAME</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">1ST</div>
+              <div className="leaderboard__table-item">151</div>
+              <div className="leaderboard__table-item">CALLA</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">2ND</div>
+              <div className="leaderboard__table-item">143</div>
+              <div className="leaderboard__table-item">KEVIN</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">3RD</div>
+              <div className="leaderboard__table-item">116</div>
+              <div className="leaderboard__table-item">SAM</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">4TH</div>
+              <div className="leaderboard__table-item">113</div>
+              <div className="leaderboard__table-item">CHRIS</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">5TH</div>
+              <div className="leaderboard__table-item">101</div>
+              <div className="leaderboard__table-item">KENNY</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">6TH</div>
+              <div className="leaderboard__table-item">98</div>
+              <div className="leaderboard__table-item">CALLA</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">7TH</div>
+              <div className="leaderboard__table-item">80</div>
+              <div className="leaderboard__table-item">KEVIN</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">8TH</div>
+              <div className="leaderboard__table-item">79</div>
+              <div className="leaderboard__table-item">SAM</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">9TH</div>
+              <div className="leaderboard__table-item">75</div>
+              <div className="leaderboard__table-item">CHRIS</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">10TH</div>
+              <div className="leaderboard__table-item">71</div>
+              <div className="leaderboard__table-item">KENNY</div>
+            </div>
+          </div>
         </div>
       )
     } else if (boardType === 'multi') {
       return (
         <div className="leaderboard__board-container">
-          multi
+          <div className="leaderboard__table">
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">RANK</div>
+              <div className="leaderboard__table-item">WINS</div>
+              <div className="leaderboard__table-item">NAME</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">1ST</div>
+              <div className="leaderboard__table-item">151</div>
+              <div className="leaderboard__table-item">CALLA</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">2ND</div>
+              <div className="leaderboard__table-item">143</div>
+              <div className="leaderboard__table-item">KEVIN</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">3RD</div>
+              <div className="leaderboard__table-item">116</div>
+              <div className="leaderboard__table-item">SAM</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">4TH</div>
+              <div className="leaderboard__table-item">113</div>
+              <div className="leaderboard__table-item">CHRIS</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">5TH</div>
+              <div className="leaderboard__table-item">101</div>
+              <div className="leaderboard__table-item">KENNY</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">6TH</div>
+              <div className="leaderboard__table-item">98</div>
+              <div className="leaderboard__table-item">CALLA</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">7TH</div>
+              <div className="leaderboard__table-item">80</div>
+              <div className="leaderboard__table-item">KEVIN</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">8TH</div>
+              <div className="leaderboard__table-item">79</div>
+              <div className="leaderboard__table-item">SAM</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">9TH</div>
+              <div className="leaderboard__table-item">75</div>
+              <div className="leaderboard__table-item">CHRIS</div>
+            </div>
+            <div className="leaderboard__table-row">
+              <div className="leaderboard__table-item">10TH</div>
+              <div className="leaderboard__table-item">71</div>
+              <div className="leaderboard__table-item">KENNY</div>
+            </div>
+          </div>
         </div>
       )
     }
@@ -26,15 +139,15 @@ class LeaderboardIndex extends Component {
     if (boardType === 'single') {
       return (
         <div className="leaderboard__board-btn-container">
-          <a href="#" className="leaderboard__btn leaderboard__btn--active">Single Player Leaderboard</a>
-          <a href="#" className="leaderboard__btn">Multi Player Leaderboard</a>
+          <Link to="/leaderboard/single" className="leaderboard__btn leaderboard__btn--active">Single Player<br />Leaderboard</Link>
+          <Link to="/leaderboard/multi" className="leaderboard__btn">Multi Player<br />Leaderboard</Link>
         </div>
       )
     } else if (boardType === 'multi') {
       return (
         <div className="leaderboard__board-btn-container">
-          <a href="#" className="leaderboard__btn">Single Player Leaderboard</a>
-          <a href="#" className="leaderboard__btn leaderboard__btn--active">Multi Player Leaderboard</a>
+          <Link to="/leaderboard/single" className="leaderboard__btn">Single Player<br />Leaderboard</Link>
+          <Link to="/leaderboard/multi" className="leaderboard__btn leaderboard__btn--active">Multi Player<br />Leaderboard</Link>
         </div>
       )
     }
@@ -43,15 +156,18 @@ class LeaderboardIndex extends Component {
   render() {
     return (
       <div>
-        <h2 className="leaderboard__header">
-          Leaderboard Index
-        </h2> 
+        <div className="leaderboard__header-container">
+          <h2 className="leaderboard__header">
+            HIGH SCORES
+          </h2> 
+        </div>
+
+
+        {this.renderLeaderboard()}
 
         <div className="leaderboard__btn-container">
           {this.renderBoardToggleButtons()}
         </div>
-
-        {this.renderLeaderboard()}
       </div>
     )
   }
