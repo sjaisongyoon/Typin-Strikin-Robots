5c57ec3eca94a9dd73d604df0c818aca6fcdb9dd
Calla <jiyoung8751@gmail.com>
2019-07-16 08:31:02 -0700

    Change few things in session component

--- frontend/src/components/single_options/single.jsx
+++ frontend/src/components/single_options/single.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SingleOptionsForm from './single_options_form';
-import LeaderboardSingleContainer from '../leaderboard/leaderboard_single_container'
+// import LeaderboardSingleContainer from '../leaderboard/leaderboard_single_container'
 
 class Single extends React.Component {
     render () {
@@ -10,7 +10,7 @@ class Single extends React.Component {
                     Single Player MODE 
                 </div>
                 <div className="single__leaderboard">
-                    <LeaderboardSingleContainer />
+                    {/* <LeaderboardSingleContainer /> */}
                 </div>
                 <div className="single__options">
                     <SingleOptionsForm />
