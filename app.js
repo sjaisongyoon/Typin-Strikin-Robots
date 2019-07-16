cfb53b82375e511cacda8a1028566fd34310d4d2
kallmeehkev <kevin.la79@gmail.com>
2019-07-15 17:42:55 -0700

    Add socket functionality to multi game component

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 const users = require("./routes/api/users");
 const matches = require("./routes/api/matches");
 const multiplayerGameRooms = require("./routes/api/multiplayerGameRooms");
@@ -28,3 +31,26 @@ app.use("/api/multiplayerGameRooms", multiplayerGameRooms);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+let socketList = {};
+
+io.on('connection', socket => {
+    console.log('A user has connected');
+    socket.id = Math.random();
+    socket.health = 100;
+    socketList[socket.id] = socket;
+
+    socket.on("gameroom", data => {
+        io.emit('gameroom', data);
+        console.log(data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log("user disconnected");
+        delete socketList[socket.id];
+    });
+});
+
+http.listen(3001, () => {
+    console.log('listening on 3001')
+})
--- frontend/src/components/multi_game/multi_game.jsx
+++ frontend/src/components/multi_game/multi_game.jsx
@@ -1,6 +1,49 @@
 import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client';
 
 class MultiGame extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ownHealthBar: 100,
+      enemyHealthBar: 100,
+      socket: socketIOClient("http://127.0.0.1:3001")
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.openSocket = this.openSocket.bind(this);
+  }
+
+  openSocket() {
+    this.state.socket.on("gameroom", data => {
+      if (data.myUserId !== this.props.currentUser.id) {
+        this.setState({ ownHealthBar: data.enemyHealthBar })
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.openSocket();
+    console.log('open socket')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.ownHealthBar !== this.state.ownHealthBar) {
+      this.setState({ownHealthBar: this.state.ownHealthBar})
+    }
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    let newEnemyHealthBar = this.state.enemyHealthBar - 10;
+    let data = {
+      enemyHealthBar: newEnemyHealthBar,
+      myUserId: this.props.currentUser.id
+    }
+    this.state.socket.emit("gameroom", data)
+    this.setState({enemyHealthBar: newEnemyHealthBar})
+  }
+
+
   render() {
     return (
       <div className="multigame__container">
@@ -25,6 +68,10 @@ class MultiGame extends Component {
         <div className="multigame__fight-container">
           <p>
             Image goes here...
+            <div>
+              myhealthbar: {this.state.ownHealthBar} enemyHealthBar: {this.state.enemyHealthBar}
+              <button onClick={this.handleClick}>Hit</button>
+            </div>
           </p> 
         </div>
         <div className="game__input-container">
