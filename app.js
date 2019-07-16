87fe6d3986ae506600167071626c96709f7ae011
Calla <jiyoung8751@gmail.com>
2019-07-16 13:57:13 -0700

    Change app file

--- app.js
+++ app.js
@@ -56,3 +56,10 @@ http.listen(3001, () => {
 })
 
 const path = require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -18,12 +18,6 @@ import Modal from './modal/modal';
 
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
 
 const App = () => (
     <div>
