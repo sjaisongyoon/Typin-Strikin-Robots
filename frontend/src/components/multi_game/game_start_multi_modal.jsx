365d46d1cdeabc4a8202f7876c5b7a11dbc60d0e
crgee1 <christopherrgee@gmail.com>
2019-07-16 12:50:50 -0700

    Fix formatting

--- frontend/src/components/multi_game/game_start_multi_modal.jsx
+++ frontend/src/components/multi_game/game_start_multi_modal.jsx
@@ -2,19 +2,34 @@ import React from 'react'
 
 class GameStartMultiModal extends React.Component {
   constructor(props) {
-    super(props)
-
+    super(props);
+    this.state = { time: 3 }
   }
 
-  componentDidMount() {
+  countdown() {
+    let timer = setInterval(() => {
+      if (this.state.time > 1) {
+        this.setState((prevState) => ({
+          time: prevState.time - 1,
+        }))
+      } else if (this.state.time === 1) {
+        this.setState({ time: 'TYPE!!!!!' })
 
+        // console.log(this.state);
+        // this.props.closeModal();
+      } else {
+        clearInterval(timer);
+      }
+    }, 1000);
   }
-
-  
 
   render() {
     return (
-      <div></div>
+      <div className='gamestart-single__modal-container'>
+        <div className='gamestart-single__modal-header'>
+          {this.state.time}
+        </div>
+      </div>
     )
   }
 }
--- frontend/src/components/single_game/game_start_single_modal.jsx
+++ frontend/src/components/single_game/game_start_single_modal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 class GameStartSingleModal extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = { time: 3 }
 
   }
@@ -18,10 +18,7 @@ class GameStartSingleModal extends React.Component {
           time: prevState.time - 1,
         }))
       } else if (this.state.time === 1){
-        this.setState({time: 'TYPE!!!!!'})
-        
-        // console.log(this.state);
-        // this.props.closeModal();
+        this.setState({time: 'TYPE!!!!!'});
       } else {
         clearInterval(timer);
       }
