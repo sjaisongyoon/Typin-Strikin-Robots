585244c32f28a2d0804e6aa28504baa45b7d225b
Sam Yoon <sjaisongyoon@gmail.com>
2019-08-05 15:04:39 -0700

    Add activeGameRoom to UI slice

--- frontend/src/reducers/ui_reducer.js
+++ frontend/src/reducers/ui_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import modalReducer from './modal_reducer';
+import activeGameRoomReducer from './active_game_room_reducer'
 
 const uiReducer = combineReducers({
-  modal: modalReducer
+  modal: modalReducer,
+  activeGameRoom: activeGameRoomReducer
 });
 
 export default uiReducer;
